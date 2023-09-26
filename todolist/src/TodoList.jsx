744653c42d00848b70b0d659bc09601b99773763
Olli Salo <olliasalo@outlook.com>
2023-09-26 15:07:37 +0300

    added delete function

--- todolist/src/TodoList.jsx
+++ todolist/src/TodoList.jsx
@@ -12,6 +12,11 @@ function TodoList() {
     setTodos([...todos, todo]);
   }
 
+  const deleteTodo = (index) => {
+    setTodos(prevTodos => prevTodos.filter((todo, i) => i !== index));
+    
+  }
+
   return (
     <div>
         Add todo: <br></br>
@@ -23,7 +28,12 @@ function TodoList() {
         <tbody>
             <tr><th>Date</th><th>Description</th></tr>
         {
-          todos.map((todo, index) => <tr key={index}><td>{todo.date}</td><td>{todo.description}</td></tr>)
+          todos.map((todo, index) => 
+            <tr key={index}>
+                <td>{todo.date}</td>
+                <td>{todo.description}</td> 
+                <td><button onClick={() => deleteTodo(index)}>Delete</button></td>
+            </tr>)
         }
         </tbody>
       </table>   
